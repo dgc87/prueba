839153d8cf4fe36c2df456342e8ae88f0f1f7bbd
david <david.garcia.castao@gmail.com>
2020-02-19 20:12:51 +0100

    Ficheros de prueba

---
+++ src/Ejercicio1.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio1 {
+
+		public static void main(String[] args) {
+			 int i=0;
+
+			 while(i++<10){
+
+			 if (++i%3!=0) ;
+
+			 System.out.print (" El numero es "+i);
+
+			 }
+		}
+	}
